9150ecffae34ac156bcad93713a7802967f14a75
wyattsullivan <wsull001@ucr.edu>
2018-01-29 16:46:01 -0800

    Adding DbHelper, updating Product

---
+++ app/src/main/java/com/example/wyattsullivan/fridgeio/DbHelper.java
@@ -0,0 +1,43 @@
+package com.example.wyattsullivan.fridgeio;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by wyattsullivan on 1/29/18.
+ */
+
+public class DbHelper extends SQLiteOpenHelper {
+    public static final String dbName = "FridgeIo.db";
+
+
+    public DbHelper(Context context) {
+        super(context, dbName, null, 1);
+        SQLiteDatabase db = this.getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //create the ProductList
+        db.execSQL("CREATE TABLE ProductList (prodID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                                            "name TEXT, " +
+                                                            "FridgeID INTEGER, " +
+                                                            "description TEXT," +
+                                                            "fullness INTEGER," +
+                                                            "expDate DATE)");
+        db.execSQL("CREATE TABLE IF NOT EXISTS ToUpdate (updateID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                                         "type INTEGER, " +
+                                                         "prodID INTEGER," +
+                                                         "FOREIGN KEY (prodID) REFERENCES ProductList(prodID))");
+        db.execSQL("CREATE TABLE IF NOT EXISTS GroceryList (grocID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                                            "name TEXT, " +
+                                                            "quantity INT, " +
+                                                            "notes TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
