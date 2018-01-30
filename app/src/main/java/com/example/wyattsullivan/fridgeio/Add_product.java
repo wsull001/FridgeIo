2adfd2a0dd307afef19c194c4a12203f4a34c50b
wyattsullivan <wsull001@ucr.edu>
2018-01-29 19:07:27 -0800

    Adding Add Product Activity

---
+++ app/src/main/java/com/example/wyattsullivan/fridgeio/Add_product.java
@@ -0,0 +1,13 @@
+package com.example.wyattsullivan.fridgeio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Add_product extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product);
+    }
+}
