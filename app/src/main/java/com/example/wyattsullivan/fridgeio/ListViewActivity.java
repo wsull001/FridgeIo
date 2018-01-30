052af6d2126a1dd365a43159ca40f1ac508b49ee
wyattsullivan <wsull001@ucr.edu>
2018-01-29 20:26:24 -0800

    added add button to layout

--- app/src/main/java/com/example/wyattsullivan/fridgeio/ListViewActivity.java
+++ app/src/main/java/com/example/wyattsullivan/fridgeio/ListViewActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -44,6 +45,12 @@ public class ListViewActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar_menu, menu);
+        return true;
     }
 
 }
