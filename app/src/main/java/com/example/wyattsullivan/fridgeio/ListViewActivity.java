91180963e5afae220c5d836adb0432172903a286
samhwangg <samuel.hwang726@gmail.com>
2018-01-29 19:48:57 -0800

    Changed activity_main to activity_listView and MainActivity to ListViewActivity

--- app/src/main/java/com/example/wyattsullivan/fridgeio/ListViewActivity.java
+++ app/src/main/java/com/example/wyattsullivan/fridgeio/ListViewActivity.java
@@ -7,9 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-import java.util.Date;
-
-public class MainActivity extends AppCompatActivity {
+public class ListViewActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
     private DbHelper mydb;
@@ -37,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_listView);
 
         mydb = new DbHelper(this);
 
