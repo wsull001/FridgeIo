c2b013284f5c77cc26022f568c722c6ef42e9177
samhwangg <samuel.hwang726@gmail.com>
2018-02-12 14:38:17 -0800

    Renamed fragment directories and uploaded settings icon

--- app/src/main/java/com/example/wyattsullivan/fridgeio/Fragment_Settings.java
+++ app/src/main/java/com/example/wyattsullivan/fridgeio/Fragment_Settings.java
@@ -10,9 +10,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class Fragment_Bluetooth extends Fragment {
-    public static Fragment_Bluetooth newInstance() {
-        Fragment_Bluetooth fragment = new Fragment_Bluetooth();
+public class Fragment_Settings extends Fragment {
+    public static Fragment_Settings newInstance() {
+        Fragment_Settings fragment = new Fragment_Settings();
         return fragment;
     }
 
@@ -24,6 +24,6 @@ public class Fragment_Bluetooth extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_bluetooth, container, false);
+        return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 }
